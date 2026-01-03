f86b1148f78e96e1b0c07ea763d26842f069b711
Guru Charan Nalabolu <nalabolugurucharan12@gmail.com>
2026-01-03 10:15:04 +0530

    add session-based login and logout

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
+
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -10,15 +12,27 @@ def login():
 
         # TEMP logic (real auth later)
         if username == "admin" and password == "1234":
+            session["user"] = username   # to store login
             return redirect(url_for("dashboard"))
         else:
             return "Invalid credentials"
 
     return render_template("login.html")
 
+
 @app.route("/dashboard")
 def dashboard():
+    if "user" not in session:
+        return redirect(url_for("login"))
+    
     return render_template("dashboard.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
